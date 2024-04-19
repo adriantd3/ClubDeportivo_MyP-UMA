c6e4a36b1ec670bc71799c5c3185adadec3e36cd
adriantd3 <adriantorremocha@hotmail.com>
2024-04-20 01:42:29 +0200

    EvolutionaryAlgorithm - EATest done
    - Small fix on EvolutionaryAlgorithm
    
    -Tests with 100% coverage done

--- EvolutionaryAlgorithm/src/main/java/org/mps/EvolutionaryAlgorithm.java
+++ EvolutionaryAlgorithm/src/main/java/org/mps/EvolutionaryAlgorithm.java
@@ -4,6 +4,8 @@ import org.mps.crossover.CrossoverOperator;
 import org.mps.mutation.MutationOperator;
 import org.mps.selection.SelectionOperator;
 
+import java.util.Arrays;
+
 /**
  * La clase EvolutionaryAlgorithm representa un algoritmo evolutivo básico que
  * se utiliza para resolver problemas de optimización.
@@ -48,7 +50,10 @@ public class EvolutionaryAlgorithm {
         return mutationOperator;
     }
 
-    public void setMutationOperator(MutationOperator mutationOperator) {
+    public void setMutationOperator(MutationOperator mutationOperator) throws EvolutionaryAlgorithmException {
+        if(mutationOperator == null){
+            throw new EvolutionaryAlgorithmException();
+        }
         this.mutationOperator = mutationOperator;
     }
 
@@ -57,7 +62,7 @@ public class EvolutionaryAlgorithm {
         if (population != null && population.length > 0 && population[0]!=null && population[0].length>0) {
             // Creamos una nueva población para los descendientes
             //FIX: tamaño del subarray deberia ser population[0].length?
-            int[][] offspringPopulation = new int[population.length][population.length];
+            int[][] offspringPopulation = new int[population.length][population[0].length];
 
             // Aplicamos operadores de selección y cruce para generar descendientes
             // FIX: si el tamaño de population es impar dara indexoutofbounds
@@ -76,6 +81,9 @@ public class EvolutionaryAlgorithm {
             for (int i = 0; i < offspringPopulation.length; i++) {
                 offspringPopulation[i] = mutationOperator.mutate(offspringPopulation[i]);
             }
+
+            System.out.println(Arrays.toString(offspringPopulation[0]));
+            System.out.println(Arrays.toString(offspringPopulation[1]));
 
             // Reemplazo
             for (int i = 0; i < population.length; i++) {
@@ -108,7 +116,10 @@ public class EvolutionaryAlgorithm {
         return this.selectionOperator;
     }
 
-    public void setSelectionOperator(SelectionOperator selectionOperator) {
+    public void setSelectionOperator(SelectionOperator selectionOperator) throws EvolutionaryAlgorithmException {
+        if(selectionOperator == null){
+            throw new EvolutionaryAlgorithmException();
+        }
         this.selectionOperator = selectionOperator;
     }
 
@@ -116,7 +127,10 @@ public class EvolutionaryAlgorithm {
         return this.crossoverOperator;
     }
 
-    public void setCrossoverOperator(CrossoverOperator crossoverOperator) {
+    public void setCrossoverOperator(CrossoverOperator crossoverOperator) throws EvolutionaryAlgorithmException {
+        if(crossoverOperator == null){
+            throw new EvolutionaryAlgorithmException();
+        }
         this.crossoverOperator = crossoverOperator;
     }
 
--- EvolutionaryAlgorithm/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
+++ EvolutionaryAlgorithm/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
@@ -1,11 +1,14 @@
 package org.mps;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mps.crossover.OnePointCrossover;
 import org.mps.mutation.SwapMutation;
 import org.mps.selection.TournamentSelection;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -80,24 +83,154 @@ public class EvolutionaryAlgorithmTest {
     @Nested
     @DisplayName("optimize method")
     class Optimize {
+        @BeforeEach
+        public void setup(){
+            OnePointCrossover crossover = new OnePointCrossover();
+            SwapMutation mutation = new SwapMutation();
+            try {
+                TournamentSelection selection = new TournamentSelection(5);
 
+                assertDoesNotThrow(() -> {
+                    evolutionaryAlgorithm = new EvolutionaryAlgorithm(selection, mutation, crossover);
+                });
+            } catch (Exception e) {
+            }
+        }
+
+        @DisplayName("null population")
+        @Test
+        public void Optimize_NullPopulation_ThrowsEvolutionaryAlgorithmException() {
+            int [][] population = null;
+
+            assertThrows(EvolutionaryAlgorithmException.class, () ->{
+               evolutionaryAlgorithm.optimize(population);
+            });
+        }
+
+        @DisplayName("empty population")
+        @Test
+        public void Optimize_EmptyPopulation_ThrowsEvolutionaryAlgorithmException(){
+            int [][] population = new int[0][0];
+
+            assertThrows(EvolutionaryAlgorithmException.class, () ->{
+                evolutionaryAlgorithm.optimize(population);
+            });
+        }
+
+        @DisplayName("null population subarray")
+        @Test
+        public void Optimize_NullPopulationSubarray_ThrowsEvolutionaryAlgorithmException(){
+            int [][] population = new int[1][];
+
+            assertThrows(EvolutionaryAlgorithmException.class, () ->{
+                evolutionaryAlgorithm.optimize(population);
+            });
+        }
+
+        @DisplayName("empty population subarray")
+        @Test
+        public void Optimize_EmptyPopulationSubarray_ThrowsEvolutionaryAlgorithmException(){
+            int [][] population = new int[1][0];
+
+            assertThrows(EvolutionaryAlgorithmException.class, () ->{
+                evolutionaryAlgorithm.optimize(population);
+            });
+        }
+
+        @DisplayName("returns altered population")
+        @Test
+        public void Optimize_InRangeParameters_AltersPopulation() throws EvolutionaryAlgorithmException {
+            int [][] population = new int[][] {{7,7,7,7},{8,8,8,8}};
+
+            population = evolutionaryAlgorithm.optimize(population);
+
+            boolean hasChanged = false;
+            for (int i = 0; i < population[0].length; i++){
+                if (population[0][i] != 7 || population[1][i] != 8) {
+                    hasChanged = true;
+                    break;
+                }
+            }
+            assertTrue(hasChanged);
+        }
     }
-
     @Nested
-    @DisplayName("Selection operator")
-    class SelectionOperator {
+    @DisplayName("set methods")
+    class Setters{
 
+        @BeforeEach
+        public void setup(){
+            OnePointCrossover crossover = new OnePointCrossover();
+            SwapMutation mutation = new SwapMutation();
+            try {
+                TournamentSelection selection = new TournamentSelection(5);
+
+                assertDoesNotThrow(() -> {
+                    evolutionaryAlgorithm = new EvolutionaryAlgorithm(selection, mutation, crossover);
+                });
+            } catch (Exception e) {
+            }
+        }
+
+        @DisplayName("null selection operator")
+        @Test
+        public void SetSelection_NullSelectionOperator_ThrowsEvolutionaryAlgorithmException() {
+            TournamentSelection selection = null;
+
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                evolutionaryAlgorithm.setSelectionOperator(selection);
+            });
+        }
+
+        @DisplayName("non null selection operator")
+        @Test
+        public void SetSelection_NonNullSelectionOperator_UpdatesOperator() throws EvolutionaryAlgorithmException {
+            TournamentSelection selection = new TournamentSelection(10);
+
+            evolutionaryAlgorithm.setSelectionOperator(selection);
+
+            assertEquals(selection,evolutionaryAlgorithm.getSelectionOperator());
+        }
+
+        @DisplayName("null mutation operator")
+        @Test
+        public void SetMutation_NullMutationOperator_ThrowsEvolutionaryAlgorithmException() {
+            SwapMutation mutation = null;
+
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                evolutionaryAlgorithm.setMutationOperator(mutation);
+            });
+        }
+
+        @DisplayName("non null mutation operator")
+        @Test
+        public void SetMutation_NonNullMutationOperator_UpdatesOperator() throws EvolutionaryAlgorithmException {
+            SwapMutation mutation = new SwapMutation();
+
+            evolutionaryAlgorithm.setMutationOperator(mutation);
+
+            assertEquals(mutation,evolutionaryAlgorithm.getMutationOperator());
+        }
+
+        @DisplayName("null mutation operator")
+        @Test
+        public void SetCrossover_NullCrossoverOperator_ThrowsEvolutionaryAlgorithmException() {
+            OnePointCrossover crossover = null;
+
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                evolutionaryAlgorithm.setCrossoverOperator(crossover);
+            });
+        }
+
+        @DisplayName("non null mutation operator")
+        @Test
+        public void SetCrossover_NonNullCrossoverOperator_UpdatesOperator() throws EvolutionaryAlgorithmException {
+            OnePointCrossover crossover = new OnePointCrossover();
+
+            evolutionaryAlgorithm.setCrossoverOperator(crossover);
+
+            assertEquals(crossover,evolutionaryAlgorithm.getCrossoverOperator());
+        }
     }
 
-    @Nested
-    @DisplayName("Mutation operator")
-    class MutationOperator {
-
-    }
-
-    @Nested
-    @DisplayName("Crossover operator")
-    class CrossoverOperator {
-
-    }
 }
